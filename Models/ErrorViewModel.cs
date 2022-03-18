f9b72824c0ea8c79c8a3fb609f3ae9c69a247a0e
Beatriz Ramos Lourenco <beatrizlourenco1912@gmail.com>
2022-03-18 11:37:54 -0300

    curso arquitetura com DDD

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace cursoApiNetcore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

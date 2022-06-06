c9efe83b6f9efb7b61b7a9caef6dc79b37e4dcee
Beatriz Ramos Lourenco <beatrizlourenco1912@gmail.com>
2022-06-06 19:10:26 -0300

    ajustada consulta de usuários e referencias do projeto

--- src/Api.Application/Controllers/UsersController.cs
+++ src/Api.Application/Controllers/UsersController.cs
@@ -8,17 +8,42 @@ namespace Api.Application.Controllers
   [ApiController]
   public class UsersController : ControllerBase
   {
+    private IUserService _service;
+    public UsersController(IUserService service)
+    {
+      _service = service;
+    }
     [HttpGet]
-    public async Task<ActionResult> GetAll([FromServices] IUserService service)
+    public async Task<ActionResult> GetAll()
     {
       if (!ModelState.IsValid)
       {
-        return BadRequest(ModelState);//ModelState - verifica se o que vem é válido
+        return BadRequest(ModelState);//ModelState - verifica se o que vem é válido (bad request)
       }
 
       try
       {
-        return Ok(await service.GetAll());
+        return Ok(await _service.GetAll());
+      }
+      catch (ArgumentException ex)
+      {
+        return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+      }
+    }
+
+    //localhost:5000/api/users/{id}
+    [HttpGet]
+    [Route("{id}", Name = "GetWithId")]
+    public async Task<ActionResult> Get(Guid id)
+    {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      try
+      {
+        return Ok(await _service.Get(id));
       }
       catch (ArgumentException ex)
       {

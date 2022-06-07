632537577b7d0b56393a84e01e5470da4f25ad34
Beatriz Ramos Lourenco <beatrizlourenco1912@gmail.com>
2022-06-07 18:56:02 -0300

    add injeção de dependencia do login

--- src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
+++ src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -1,6 +1,8 @@
 using Api.Data.Context;
+using Api.Data.Implementations;
 using Api.Data.Repository;
 using Api.Domain.Interfaces;
+using Api.Domain.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,6 +14,7 @@ namespace Api.CrossCutting.DependencyInjection
     {
       serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
       serviceCollection.AddDbContext<MyContext>(options => options.UseMySql("Server=localhost;Port=3306;Database=dbarquiteturacomddd;Uid=root;Pwd=1234"));
+      serviceCollection.AddScoped<IUserRepository, UserImplementation>();
     }
   }
 }

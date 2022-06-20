e10fc7f3c596565e900236546e167fcb17fc0a11
Beatriz Ramos Lourenco <beatrizlourenco1912@gmail.com>
2022-06-20 06:59:36 -0300

    correção na migration, para sql server

--- src/Api.Data/Context/ContextFactory.cs
+++ src/Api.Data/Context/ContextFactory.cs
@@ -8,9 +8,16 @@ namespace Api.Data.Context
     public MyContext CreateDbContext(string[] args)
     {
       //Usado para a criação de migrations
-      var connectionString = "Server=localhost;Port=3306;Database=dbarquiteturacomddd;Uid=root;Pwd=1234";
+
+      //MySQL
+      // var connectionString = "Server=localhost;Port=3306;Database=dbarquiteturacomddd;Uid=root;Pwd=1234";
+      // optionsbuilder.UseMySql(connectionString);
+
+      //SQL Server
+      var connectionString = "Server=BEATRIZ\\SQLEXPRESS;Database=dbarquiteturacomddd;MultipleActiveResultSets=true;User ID=sa;Password=1234";
       var optionsbuilder = new DbContextOptionsBuilder<MyContext>();
-      optionsbuilder.UseMySql(connectionString);
+
+      optionsbuilder.UseSqlServer(connectionString);
 
       return new MyContext(optionsbuilder.Options);
     }

d7aaa2cf3074f1f8f9628415443d44a182965087
Beatriz Ramos Lourenco <beatrizlourenco1912@gmail.com>
2022-06-28 07:30:08 -0300

    Add CEP e Municipio DTOs

---
+++ src/Api.Domain/Dtos/Municipio/MunicipioDTOCompleto.cs
@@ -0,0 +1,13 @@
+using System;
+using Api.Domain.Dtos.UF;
+
+namespace Api.Domain.Dtos.Municipio
+{
+  public class MunicipioDTOCompleto
+  {
+    public string Nome { get; set; }
+    public int CodIBGE { get; set; }
+    public Guid UfId { get; set; }
+    public UfDTO Uf { get; set; }
+  }
+}
